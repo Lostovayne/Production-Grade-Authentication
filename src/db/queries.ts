1cc7fca06c3bf7fdb914b16d327a07bbd3b63bb8
LostoVayne <moises.barrera.dev@gmail.com>
2025-09-28 07:02:22 -0300

    Reorder imports and fix indentation in queries.ts

--- src/db/queries.ts
+++ src/db/queries.ts
@@ -1,18 +1,18 @@
-import { db } from "@/db/db";
-import { todos } from "./schema";
 import { eq } from "drizzle-orm";
+import { db } from "@/db/db";
 import type { NewTodo } from "@/types";
+import { todos } from "./schema";
 
 export const insertTodo = async (todo: NewTodo) => {
-  const [result] = await db.insert(todos).values(todo).returning();
-  return result;
+	const [result] = await db.insert(todos).values(todo).returning();
+	return result;
 };
 
 export const getTodosByUserId = async (userId: string) => {
-  const todoList = await db
-    .select()
-    .from(todos)
-    .where(eq(todos.userId, userId))
-    .orderBy(todos.createdAt);
-  return todoList;
+	const todoList = await db
+		.select()
+		.from(todos)
+		.where(eq(todos.userId, userId))
+		.orderBy(todos.createdAt);
+	return todoList;
 };
